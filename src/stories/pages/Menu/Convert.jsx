fbeba7a32fc200102fdfadc4eb8690ee2b6197d3
ohjun <ojjun4527@gmail.com>
2024-10-20 00:13:08 +0900

    connect api with GradeRegister

---
+++ src/stories/pages/Menu/Convert.jsx
@@ -0,0 +1,19 @@
+const Convert = ({ selectedYear, selectedTerm }) => {
+    const yearMapping = {
+        2018: 0,
+        2019: 2,
+        2020: 4,
+        2021: 6,
+        2022: 8,
+        2023: 10,
+        2024: 12,
+    };
+    const termMapping = {
+        1: 0,
+        2: 1,
+    };
+    let result = yearMapping[selectedYear] + termMapping[selectedTerm];
+    return result;
+};
+
+export default Convert;
