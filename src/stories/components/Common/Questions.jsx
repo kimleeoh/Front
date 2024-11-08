514b6810e9aa7f10210bd321738621b613a96314
yeunsuh <yeunsuh.kim@gmail.com>
2024-11-09 03:06:47 +0900

    trivial

--- src/stories/components/Common/Questions.jsx
+++ src/stories/components/Common/Questions.jsx
@@ -234,7 +234,7 @@ Questions.propTypes = {
     time: PropTypes.string.isRequired,
     views: PropTypes.number.isRequired,
     like: PropTypes.number.isRequired,
-    img: PropTypes.array,
+    img: PropTypes.string,
     limit: PropTypes.number.isRequired,
     point: PropTypes.number.isRequired,
 };
