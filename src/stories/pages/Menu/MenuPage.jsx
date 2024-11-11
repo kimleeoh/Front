c48c0a3a16e7fa85aaa879173b02fbd940a2db21
yeunsuh <yeunsuh.kim@gmail.com>
2024-11-11 12:13:46 +0900

    done

--- src/stories/pages/Menu/MenuPage.jsx
+++ src/stories/pages/Menu/MenuPage.jsx
@@ -27,33 +27,40 @@ const MenuPage = () => {
     const [modalNotifyContent, setModalNotifyContent] = useState(null);
     const [currentModalIndex, setCurrentModalIndex] = useState(0);
     const [totalModalNotifyContent, setTotalModalNotifyContent] = useState(null);
+    const [isFetching, setIsFetching] = useState(false);
 
     // 페이지 첫 로드 시 /api/modal-notify 요청
     const fetchModalNotifyContent = useCallback(async() => {
+        if (isFetching) return; // Prevent multiple calls
+
+        setIsFetching(true);
+            
         try {
             const response = await BaseAxios.get("/api/modal-notify");
             const data = response.data;
             console.log("modal-notify data: ", data);
 
-            if (data && Array.isArray(data)) {
+            if (data && Array.isArray(data)&& data[0]!==undefined) {
                 setTotalModalNotifyContent(data);
                 setCurrentModalIndex(0);
                 
                 setModalNotifyContent(data[0]);
-
+                console.log(modalNotifyContent);
                 modalNotifyRef.current.open();
             }
         } catch (error) {
             console.error("Failed to fetch modal content:", error);
+        }finally {
+            setIsFetching(false);
         }
-    },[]);
+    },[isFetching]);
 
     useEffect(() => {
         if (!userData && !isLoading && !error) {
             fetchUserData();
         }
 
-        fetchModalNotifyContent();
+        if(modalNotifyContent==null)fetchModalNotifyContent();
     }, [userData, isLoading, error, fetchUserData]);
 
     const { name, profile_Img } = useMemo(
