efb32c9b66e634748edb1ffc268f8c4a779e3eb4
ohjun <ojjun4527@gmail.com>
2024-11-16 17:20:38 +0900

    modify HomePage

--- src/stories/pages/Home/HomePage.jsx
+++ src/stories/pages/Home/HomePage.jsx
@@ -31,15 +31,81 @@ const HomePage = () => {
 
     const [originPoint, setOriginPoint] = useState(null);
     const [hasNewNotification, setHasNewNotification] = useState(false);
-    const [answerablePosts, setAnswerablePosts] = useState([]);
-    const [trendingTips, settrendingTips] = useState([]);
-    const [trendingQna, settrendingQna] = useState([]);
+    //const [answerablePosts, setAnswerablePosts] = useState([]);
+    //const [trendingTips, settrendingTips] = useState([]);
+    //const [trendingQna, settrendingQna] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
     const [modalNotifyContent, setModalNotifyContent] = useState(null);
     const [totalModalNotifyContent, setTotalModalNotifyContent] = useState([]);
     const [currentModalIndex, setCurrentModalIndex] = useState(0);
     const modalNotifyRef = useRef();
+
+    const answerablePosts = [
+        {
+            title: "내년에 들을까 하는데 추천하시나요",
+            content: "제곧내",
+            time: "2024-10-31T03:15:17.872+00:00", // Example ISO timestamp
+            views: 5,
+        },
+        {
+            title: "모델링 관련 질문 드립니다.",
+            content: `HDRI 파일을 다운 받았는데 왜 안나오는지 모르겠어요..!
+                      적용하는 방법 알려주시면 감사하겠습니다ㅜㅜ`,
+            time: "2024-11-04T03:29:46.538+00:00",
+            views: 14,
+        },
+        {
+            title: "삼디 왜 이렇게 되는거죠.",
+            content: `이거 왜 투명도 설정 안되는거죠..??? 이해가 안가는군요`,
+            time: "2024-11-05T11:38:15.260+00:00",
+            views: 50,
+        },
+    ];
+
+    const trendingTips = [
+        {
+            title: "컴시개 필기본 (A+ 맞음)",
+            content: `컴시개 필기 필요한 사람`,
+            time: "2024-11-07T14:35:11.915+00:00", // Example ISO timestamp
+            views: 6,
+        },
+        {
+            title: "삼디 들으려는 친구들은 보아라",
+            content: `삼디 뭐 공부할지 고민하는 사람`,
+            time: "2024-11-09T11:52:44.406+00:00", // Example ISO timestamp
+            views: 5,
+        },
+    ]
+
+    const trendingQna = [
+        {
+            title: "삼디 왜 이렇게 되는거죠",
+            content: `이거 왜 투명도 설정 안되는거죠..??? 이해가 안가는군요`,
+            time: "2024-11-05T11:38:15.260+00:00", // Example ISO timestamp
+            views: 50,
+        },
+        {
+            title: "모델링 관련 질문 드립니다",
+            content: `HDRI 파일을 다운 받았는데 왜 안나오는지 모르겠어요..!
+적용하는 방법 알려주시면 감사하겠습니다ㅜㅜ`,
+            time: "2024-11-04T03:29:46.538+00:00", // Example ISO timestamp
+            views: 14,
+        },
+        {
+            title: "라이팅 관련 질문 드려요!",
+            content: `HDRI 파일 다운을 했는데 적용이 안돼요..
+하는 방법 알려주세요!`,
+            time: "2024-11-04T03:31:22.459+00:00", // Example ISO timestamp
+            views: 20,
+        },
+        {
+            title: "자구실..",
+            content: `수업을 좀 빼먹었는데 지금 나가고 있는 진도가 어디인가요..? 작게 사례하겠습니다`,
+            time: "2024-11-09T15:35:40.366+00:00", // Example ISO timestamp
+            views: 24,
+        },
+    ]
 
     useEffect(() => {
         const fetchData = async () => {
@@ -50,64 +116,64 @@ const HomePage = () => {
                 const [
                     pointResponse,
                     notificationResponse,
-                    trendingResponse,
+                    //trendingResponse,
                     answerablePostsResponse,
                     modalResponse,
                 ] = await Promise.all([
                     BaseAxios.get("/api/h/point"),
                     BaseAxios.get("/api/h/notify/new", { send: false }),
-                    BaseAxios.post("/api/h/home/trending"),
+                    //BaseAxios.post("/api/h/home/trending"),
                     BaseAxios.post("/api/h/home/answer-possible"),
                     BaseAxios.get("/api/h/modal-notify"),
                 ]);
 
-                const { trendingTipsResponse, trendingQnaResponse } =
-                    trendingResponse.data;
+                // const { trendingTipsResponse, trendingQnaResponse } =
+                //     trendingResponse.data;
 
                 setOriginPoint(pointResponse.data.point);
                 setHasNewNotification(notificationResponse.data.newNotify);
 
-                if (Array.isArray(answerablePostsResponse.data)) {
-                    setAnswerablePosts(answerablePostsResponse.data);
-                } else {
-                    console.error(
-                        "Unexpected data structure:",
-                        answerablePostsResponse.data
-                    );
-                    setAnswerablePosts([]);
-                }
+                // if (Array.isArray(answerablePostsResponse.data)) {
+                //     setAnswerablePosts(answerablePostsResponse.data);
+                // } else {
+                //     console.error(
+                //         "Unexpected data structure:",
+                //         answerablePostsResponse.data
+                //     );
+                //     setAnswerablePosts([]);
+                // }
 
                 // Ensure trendingTipsResponse.data is an array
-                if (Array.isArray(trendingTipsResponse)) {
-                    settrendingTips(trendingTipsResponse);
-                } else if (
-                    trendingTipsResponse &&
-                    Array.isArray(trendingTipsResponse.posts)
-                ) {
-                    settrendingTips(trendingTipsResponse.posts);
-                } else {
-                    console.error(
-                        "Unexpected data structure:",
-                        trendingTipsResponse
-                    );
-                    settrendingTips([]);
-                }
+                // if (Array.isArray(trendingTipsResponse)) {
+                //     settrendingTips(trendingTipsResponse);
+                // } else if (
+                //     trendingTipsResponse &&
+                //     Array.isArray(trendingTipsResponse.posts)
+                // ) {
+                //     settrendingTips(trendingTipsResponse.posts);
+                // } else {
+                //     console.error(
+                //         "Unexpected data structure:",
+                //         trendingTipsResponse
+                //     );
+                //     settrendingTips([]);
+                // }
 
                 // Ensure trendingQnaResponse.data is an array
-                if (Array.isArray(trendingQnaResponse)) {
-                    settrendingQna(trendingQnaResponse);
-                } else if (
-                    trendingQnaResponse &&
-                    Array.isArray(trendingQnaResponse.posts)
-                ) {
-                    settrendingQna(trendingQnaResponse.posts);
-                } else {
-                    console.error(
-                        "Unexpected data structure:",
-                        trendingQnaResponse
-                    );
-                    settrendingQna([]);
-                }
+                // if (Array.isArray(trendingQnaResponse)) {
+                //     settrendingQna(trendingQnaResponse);
+                // } else if (
+                //     trendingQnaResponse &&
+                //     Array.isArray(trendingQnaResponse.posts)
+                // ) {
+                //     settrendingQna(trendingQnaResponse.posts);
+                // } else {
+                //     console.error(
+                //         "Unexpected data structure:",
+                //         trendingQnaResponse
+                //     );
+                //     settrendingQna([]);
+                // }
 
                 const { data } = modalResponse;
                 if (data && Array.isArray(data) && data[0] !== undefined) {
